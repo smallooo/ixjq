755dc4216270ea40df3b09a40a3d57857b2395fc
dongminghe <dongming1.he@gmail.com>
2023-02-10 15:56:53 +0800

    Initial Commit

---
+++ ixqj/ixqjApp.swift
@@ -0,0 +1,20 @@
+//
+//  ixqjApp.swift
+//  ixqj
+//
+//  Created by Dongming He on 2023/2/10.
+//
+
+import SwiftUI
+
+@main
+struct ixqjApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
